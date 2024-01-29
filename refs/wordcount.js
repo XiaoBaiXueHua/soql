cc94690e39930d6ef50a557f7c03a01b3f820f1e
XiaoBaiXueHua <flaminngpancakes@gmail.com>
2024-01-29 12:10:21 -0800

    un-jQuery'd

--- refs/wordcount.js
+++ refs/wordcount.js
@@ -7,6 +7,20 @@
 // @copyright  2018+, Flamebyrd
 // @grant       none
 // ==/UserScript==
+
+const ch_head = document.querySelector(".chapter.preface.group");
+var word_count = document.querySelector("#workskin div.userstuff.module");
+if (ch_head) {
+    const color = window.getComputedStyle(document.querySelector("h3.title a")).color;
+    const style = document.createElement("style");
+    style.innerHTML = `.wordcount {font-size: 1.1em; color: ${color}; text-align: center; margin-top: 5px; font-family: Georgia, serif;}`;
+    document.querySelector("head").appendChild(style);
+    word_count = word_count.innerText.trim().split(/\S+/g).length.toLocaleString(); //won't work on cn n stuff but eh
+    const title = document.querySelector("h3.title");
+    title.insertAdjacentHTML("afterend", `<p class="wordcount">(${word_count} words)</p>`);
+}
+/*
+//original version by flamebyrd
 (function($) {
     jQuery(document).ready(function() {
         var work_wordcount = $('dd.words').text();
@@ -16,4 +30,4 @@
             $('h3.title').append('<p><span style="font-size:0.85em; color:#666666;"> (' + chapter_wordcount + ' words)</span></p>');
         }
     });
-})(jQuery);
+})(jQuery);*/
