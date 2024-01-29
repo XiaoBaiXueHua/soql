74f3b1ce1900d0636e1befe10128e921c4063d49
XiaoBaiXueHua <flaminngpancakes@gmail.com>
2024-01-28 18:22:17 -0800

    added appending buttons

--- yee/filterscript.js
+++ yee/filterscript.js
@@ -17,8 +17,11 @@ filtButt.innerHTML = `<a id="id_butt" onclick="console.log(document.querySelecto
 //const borderHover = window.getComputedStyle(document.querySelector(".actions a:hover")).borderTop;
 //const bxShad = window.getComputedStyle(document.querySelector(".actions a:hover")).boxShadow;
 //for now, rather than use js to get the colors (to match w/the skins ofc), go w/default. is tragic but it's what ao3 gets for not using :root and vars in their css
+const optWidth = window.getComputedStyle(document.querySelector("#main ul.user.navigation.actions")).width;
+console.log(parseInt(optWidth));
 const css = `
 *:not(a, #id_output, button, .current) {box-sizing: border-box;}
+* {max-width: 100%;}
 #get_id_butt {margin-right: 8px;}
 #get_id_butt:hover {cursor: pointer;}
 #id_output {width: max-content;min-width: 0; position: static;}
@@ -65,11 +68,10 @@ const css = `
 #filter_opt {
 	display: block; 
 	width: 100%; 
+	max-width: ${parseInt(optWidth)}px;
 	margin-top: 5px;
 	text-align: left;
 }
-#filter_opt p {display: block;}
-#filter_opt p:first-of-type {text-align: center;}
 #filter_opt label {
 	background: none;
 	border: none;
@@ -77,6 +79,19 @@ const css = `
 	padding: 0!important;
 	margin: 0!important;
 }
+#filter_opt .appended-tag {
+	font-size: 0.9em;
+	display: block;
+}
+#tag_actions {
+	width: 100%; 
+	margin: 5px 0;
+	display: flex;
+}
+#tag_actions > div {
+	width: 50%;
+}
+.filter-box-label {display: block;}
 @media only screen and (max-width: 48em) {
 	#error_debug > div {
 		width: 98%;
@@ -87,27 +102,27 @@ const style = document.createElement("style");
 style.innerHTML = css;
 document.querySelector("head").appendChild(style);
 
-/* saved filters current fandom checker */ 
+/* saved filters current fandom checker */
 var TAG_OWNERSHIP_PERCENT = 70; //taken from the original; seems like a good metric tbh
 var works = document.querySelector("#main.works-index");
 var form = document.querySelector("form#work-filters");
 
 var fandomName = function () {
 	var fandom = document.querySelector("#include_fandom_tags label");
-	if (!fandom) {return null;}; //if there's no fandom (like, for example, on the error page), just stop
+	if (!fandom) { return null; }; //if there's no fandom (like, for example, on the error page), just stop
 	fandom = fandom.innerText.trim();
 	//this extracts the number of works as long as it's in parentheses
 	var workNumberExtractor = /\(\d+\)/;
 	//has to be turned into a string for some reason
 	var fandomCount = fandom.match(workNumberExtractor).toString();
 	//this chops off the parentheses lol
-	fandomCount = fandomCount.substring(1, fandomCount.length-1);
+	fandomCount = fandomCount.substring(1, fandomCount.length - 1);
 	//now it's back to a number
 	fandomCount = parseInt(fandomCount);
 	//okay. so. it wants to remove any word or number between parentheses, accommodating for ampersands as well. this applies globally too so like. i hope no fandoms have parentheses in their names lol
 	//anyway the reason that the disambiguator part of the fandom tag name is getting cut off is for jjk situations, where for w/e reason the anime n manga aren't considered related fandoms and will therefore get filtered out in crossover:false, but if you care enough abt that sort of fandom, you'll probably end up checking both tags. because the local storage is otherwise sensitive to the disambiguator, you'd have to plug in and update the same filters TWICE for both tag variants otherwise.
 	const parenRem = /\((\w+(\s|&)*|\d+\s?)+\)/g;
-	fandom = fandom.replace(parenRem,"").trim();
+	fandom = fandom.replace(parenRem, "").trim();
 	console.log(`fandom: ${fandom}`);
 
 	//okay now to basically do all that but for non-fandom_ids 
@@ -116,21 +131,28 @@ var fandomName = function () {
 	workNumberExtractor = /\d+,?\d*\sW/;
 	tagCount = tagCount.match(workNumberExtractor).toString();
 	//stupid thing wants me to remove the comma too, so that's why the replace is tacked on at the end >_>
-	tagCount = tagCount.substring(0, tagCount.length-2).replace(",","");
+	tagCount = tagCount.substring(0, tagCount.length - 2).replace(",", "");
 	//now it's back to a number
 	tagCount = parseInt(tagCount);
 	console.log(`tagCount: ${tagCount}`);
 	//if for some reason, these numbers don't exist, just stop
-	if (!fandom || !fandomCount || !tagCount) {return;};
+	if (!fandom || !fandomCount || !tagCount) { return; };
 	//if a fandom has more than a (currently set to 70%) share of a particular tag, then we're in that fandom
 	return (fandomCount / tagCount * 100 > TAG_OWNERSHIP_PERCENT) ? fandom : null;
 }();
+
+var tagName = function () {
+	var tag = document.querySelector("h2.heading a.tag").innerHTML;
+	tag = tag.replace(/\((\w+\s?)+\)/, "").trim();
+	return tag;
+}();
+console.log(tagName);
 //have to have the () at the end in order to, like, Actually get the fandom name
 
 //the local storage keys
 globalKey = "filter-global";
 globalFilter = localStorage[globalKey];
-fandomKey = `filter-${fandomName}`; 
+fandomKey = `filter-${fandomName}`;
 fandomFilter = localStorage[fandomKey];
 
 /* okay now for the part where i try to recreate the autofilter submission boxes */
@@ -143,13 +165,21 @@ const searchdd = document.querySelector("dd.search:not(.autocomplete)");
 
 //because global filters should always show, they get to be on a higher level
 const globLab = document.createElement("label");
+globLab.className = "filter-box-label";
 globLab.innerHTML = "Global:";
-globLab.setAttribute("for","globalFilters");
+globLab.setAttribute("for", "globalFilters");
 const globalBox = document.createElement("textarea");
 globalBox.id = "globalFilters";
 globalBox.value = globalFilter ? globalFilter : "";
 //autosave
-globalBox.addEventListener("keyup", async () => {await localStorage.setItem(globalKey,globalBox.value)});
+globalBox.addEventListener("keyup", async () => { await localStorage.setItem(globalKey, globalBox.value) });
+//enable/disable checkbox
+const globEnab = document.createElement("input");
+globEnab.setAttribute("type","checkbox");
+globEnab.id = "enable-global";
+const gEnabLabel = document.createElement("label");
+gEnabLabel.setAttribute("for", "enable-global");
+gEnabLabel.innerHTML = "Enable";
 
 //if you fucked up the input for the saved filters, show all saved filters for double-checking; otherwise, proceed as normal
 if (!searchdd) {
@@ -162,22 +192,25 @@ if (!searchdd) {
 		var filterArray = Object.entries(localStorage);
 		for (const [key, value] of filterArray) {
 			console.log(`saved ${key}: ${value}`);
-			cssId = key.replaceAll(/\W+/g,"-");
-			const div = document.createElement("div");
-			div.id = `${cssId}-div`;
-			const label = document.createElement("label");
-			label.innerHTML = key.replace("filter-", "");
-			label.setAttribute("for", cssId);
-			const textarea = document.createElement("textarea");
-			console.log(cssId);
-			textarea.id = cssId;
-			textarea.value = value;
-			//not much point to adding a debug w/o the autosave event listener!!
-			textarea.addEventListener("keyup", async () => await localStorage.setItem(key, textarea.value));
+			//if the localStorage thing doesn't start with "filter-", then don't do anything w/it
+			if (key.toString().startsWith("filter-")) {
+				cssId = key.replaceAll(/\W+/g, "-");
+				const div = document.createElement("div");
+				div.id = `${cssId}-div`;
+				const label = document.createElement("label");
+				label.innerHTML = key.replace("filter-", "");
+				label.setAttribute("for", cssId);
+				const textarea = document.createElement("textarea");
+				console.log(cssId);
+				textarea.id = cssId;
+				textarea.value = value;
+				//not much point to adding a debug w/o the autosave event listener!!
+				textarea.addEventListener("keyup", async () => await localStorage.setItem(key, textarea.value));
 
-			div.appendChild(label);
-			div.appendChild(textarea);
-			debugDiv.appendChild(div);
+				div.appendChild(label);
+				div.appendChild(textarea);
+				debugDiv.appendChild(div);
+			}
 		}
 		document.querySelector("div.flash:not(.error)").appendChild(ohno);
 		document.querySelector("div.flash:not(.error)").appendChild(debugDiv);
@@ -190,20 +223,22 @@ if (!searchdd) {
 	summary.innerHTML = "Saved Filters";
 	const saveDiv = document.createElement("div");
 	//append the global box + label
-	saveDiv.append(globLab, globalBox);
+	saveDiv.append(globLab, globalBox, globEnab, gEnabLabel);
 	//check if this is a fandom-specific tag before making the fandom filters box
 	if (fandomName) {
 		const fanLab = document.createElement("label");
 		fanLab.innerHTML = `Fandom <small>(${fandomName})</small>:`;
-		fanLab.setAttribute("for","fandomFilters");
+		fanLab.setAttribute("for", "fandomFilters");
+		fanLab.className = "filter-box-label";
 		const fandomBox = document.createElement("textarea");
 		//give the fandom textarea an id dependent on the fandom so that perhaps later when doing the debugger syntax error screen thing, the event listeners for the autosave each have their own ids to listen to. anyway the regexp replaces all non-word chars with "-" for css compatibility
-		var cssFanName = fandomName.replaceAll(/\W+/g,"-");
-		fandomBox.id = `filter-${cssFanName}`;
+		var cssFanName = fandomName.replaceAll(/\W+/g, "-");
+		//fandomBox.id = `filter-${cssFanName}`;
+		fandomBox.id = "fandomFilters";
 		fandomBox.value = fandomFilter ? fandomFilter : "";
 		saveDiv.append(fanLab, fandomBox);
 		//add the autosave function
-		fandomBox.addEventListener("keyup", async () => {await localStorage.setItem(fandomKey, fandomBox.value)});
+		fandomBox.addEventListener("keyup", async () => { await localStorage.setItem(fandomKey, fandomBox.value) });
 	}
 	det.append(summary, saveDiv);
 	searchdd.appendChild(det);
@@ -215,57 +250,104 @@ navList.prepend(filtButt);
 
 //this is the function that gets the tag id n spits it out as an <input> element
 function nya() {
-	//add in like an options dropdown for the filer id number
-	const filterOpt = document.createElement("div");
-	filterOpt.id = "filter_opt";
-	const opt = document.createElement("p");
-	opt.innerHTML = "<strong>Options:</strong>";
-	const fil = document.createElement("p");
-	const idOutput = document.createElement("input");
-	idOutput.id = "id_output";
-	const id = document.querySelector("#favorite_tag_tag_id").value;
-	const label = document.createElement("label");
-	label.innerHTML = "filter_ids:";
-	label.setAttribute("for", "id_output");
-	//thinking of having it automatically add the "filter_ids:" thing up front, but since it also applies to user_ids bc of the subscription method, should probably make it sensitive to this sort of thing
-	idOutput.value = `${id}`;
+	//don't do anything if there's already a thing
+	if (!document.querySelector("#filter_opt")) {
+		//add in like an options dropdown for the filer id number
+		const filterOpt = document.createElement("div");
+		filterOpt.id = "filter_opt";
+		const fil = document.createElement("div");
+		const idOutput = document.createElement("input");
+		idOutput.id = "id_output";
+		//multiple ways to get an id: fave tag id (logged-in only), -> rss feed -> freeform ids (only happens when logged out bc they don't have rss feeds) -> subscribable id (this gets user/story ids)
+		var id = function () {
+			if(document.querySelector("#favorite_tag_tag_id").value) {
+				console.log("favorite tag id method")
+				return document.querySelector("#favorite_tag_tag_id").value;
+			} else if (document.querySelector("a.rss")) {
+				console.log("rss feed method");
+				var href = document.querySelector("a.rss");
+				href = href.getAttribute("href");
+				var regex = /\d+/;
+				href = href.match(regex);
+				return href;
+			} else if(document.querySelector("input [name='work_search [freeform_ids][]']:first")) {
+				return document.querySelector("input [name='work_search [freeform_ids][]']:first").value;
+			} else if(document.querySelector("#subscription_subscribable_id")) {
+				return document.querySelector("#subscription_subscribable_id").value;
+			} else {
+				alert("can't find tag id :C")
+			}
+		}();
+		const label = document.createElement("label");
+		label.innerHTML = "filter_ids:";
+		label.setAttribute("for", "id_output");
+		//thinking of having it automatically add the "filter_ids:" thing up front, but since it also applies to user_ids bc of the subscription method, should probably make it sensitive to this sort of thing
+		idOutput.value = `${id}`;
 
-	//for now, don't bother with giving the option for incl/excl fandom/global individually; if it's a fandom tag, then it'll be excluded fandomly, otherwise, globally
-	/*
-	const excl = document.createElement("button");
-	const incl = document.createElement("button");
-	excl.innerHTML = "Exclude Tag&hellip;";
-	incl.innerHTML = "Include Tag&hellip;"
+		//for now, don't bother with giving the option for incl/excl fandom/global individually; if it's a fandom tag, then it'll be excluded fandomly, otherwise, globally
+		/*
+		excl.innerHTML = "Exclude Tag&hellip;";
+		incl.innerHTML = "Include Tag&hellip;"
+		
+		function filterType(el) {
+			//buttons to show up after picking incl/excl
+			const globButt = document.createElement("button");
+			const fanButt = document.createElement("button");
+			globButt.innerHTML = "Globally";
+			fanButt.innerHTML = `For ${fandomName}`;
+			el.append(globButt, fanButt);
+			//return `${globButt.outerHTML}${fanButt.outerHTML}`;
+		}
+		excl.addEventListener("click", function () {
+			const p = document.createElement("p");
+			this.insertAdjacentHTML("afterend", filterType(p));
+			filterOpt.appendChild(p);
+		});
+		incl.addEventListener("click", function () {
+			this.insertAdjacentHTML("afterend", filterType());
+		})
+		console.log(excl); */
+		const buttonAct = document.createElement("div");
+		buttonAct.id = "tag_actions";
+		const excl = document.createElement("div");
+		const exclB = document.createElement("button");
+		exclB.innerHTML = "Exclude Tag";
+		excl.appendChild(exclB);
+		const incl = document.createElement("div");
+		const inclB = document.createElement("button");
+		inclB.innerHTML = "Include Tag";
+		incl.appendChild(inclB);
 
-	function filterType(el) {
-		//buttons to show up after picking incl/excl
-		const globButt = document.createElement("button");
-		const fanButt = document.createElement("button");
-		globButt.innerHTML = "Globally";
-		fanButt.innerHTML = `For ${fandomName}`;
-		el.append(globButt, fanButt);
-		//return `${globButt.outerHTML}${fanButt.outerHTML}`;
+		function addFilt(v) {
+			filt = fandomName ? document.querySelector("#fandomFilters") : document.querySelector("#globalFilters");
+			filt.value += ` ${v}`;
+		}
+
+		exclB.addEventListener("click", async () => {
+			addFilt(`-filter_ids:${id}`);
+			const p = document.createElement("p");
+			p.className = "appended-tag"
+			p.innerHTML = `now excluding "<strong>${tagName}</strong>" <small>(filter_ids:${id})</small> from all searches!`;
+			excl.appendChild(p);
+			//can't tell which one is gonna be changed here, so just do both
+			await localStorage.setItem(fandomKey, document.querySelector("#fandomFilters").value);
+			await localStorage.setItem(globalKey, document.querySelector("#globalFilters").value);
+		});
+		inclB.addEventListener("click", async () => {
+			addFilt(`filter_ids:${id}`);
+			await localStorage.setItem(fandomKey, document.querySelector("#fandomFilters").value);
+			await localStorage.setItem(globalKey, document.querySelector("#globalFilters").value);
+		});
+
+		//append all the things yeah
+		buttonAct.append(excl, incl);
+		fil.append(label, idOutput);
+		filterOpt.append(fil, buttonAct);
+		navList.parentElement.appendChild(filterOpt);
 	}
-	excl.addEventListener("click", function () {
-		const p = document.createElement("p");
-		this.insertAdjacentHTML("afterend", filterType(p));
-		filterOpt.appendChild(p);
-	});
-	incl.addEventListener("click", function () {
-		this.insertAdjacentHTML("afterend", filterType());
-	})
-	console.log(excl); */
-	
-	fil.append(label, idOutput, excl, incl);
-	filterOpt.append(opt, fil);
-	navList.parentElement.appendChild(filterOpt);
 };
 //so it turns out that when you do event listeners, the function does not want the parentheses after it, just the name. that's fun. would've loved to know that.
 filtButt.addEventListener("click", nya);
-//filter opt after the ul element?
-//navList.parentElement.insertAdjacentElement("afterend", filterOpt);
-//filter opt inside the ul element
-//filterOpt.innerHTML = "Options:";
 
 
 //actually it'd be kind of nice to have a thing that'll let you pick a sorting order too, except this time you have the choice to invert it
