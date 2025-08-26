da7469b96ed6c8a44495f75dbb9c98fda017af91
XiaoBaiXueHua <flaminngpancakes@gmail.com>
2025-08-26 16:55:44 -0700

    moved the extension to the top-level

--- under-construction/extension/content_scripts/autofilter-listener.js
+++
@@ -1,5 +0,0 @@
-(() => {
-	/* check and set a global guard variable, to keep it from injecting more than once */
-	if (window.hasRun) { return; }
-	window.hasRun = true;
-})
--- under-construction/extension/scripts/autofilters.js
+++
@@ -1,5 +0,0 @@
-console.log(`hi!!!!!! this is the autofilters script from the EXTENSION`)
-
-browser.tabs.executeScript({
-	file: `/content_scripts/autofilter-listener.js"`
-});
